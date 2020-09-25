52ce11ec4f3a1286a00f98c7f9c8f9665955d162
jpss22 <67197110+jpss22@users.noreply.github.com>
2020-09-25 09:31:00 -0300

    Update 1-2-calcularaposentadoria.js

--- desafios01/1-2-calcularaposentadoria.js
+++ desafios01/1-2-calcularaposentadoria.js
@@ -1,1 +1,19 @@
+const nome = "Silvana";
+const sexo = "F";
+const idade = 48;
+const contribuicao = 23;
 
+
+if( sexo === "M" && contribuicao >= 35){
+    if( (idade + contribuicao) >= 95){
+        console.log(`${nome}, você pode se aposentar!`);
+    } else{
+        console.log(`${nome}, você ainda não pode se aposentar!`);
+    }
+} else {
+    if(contribuicao >=30 && ((idade + contribuicao) >= 85)){
+        console.log(`${nome}, você pode se aposentar!`);
+    } else{
+        console.log(`${nome}, você não pode se aposentar!`);
+    }
+}
